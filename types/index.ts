d51e274c8a566ee6b0ebd15e8f6907cd9f1e7814
mikami koji <wmd147wmd@gmail.com>
2024-09-11 10:36:32 +0900

    done

---
+++ types/index.ts
@@ -0,0 +1,40 @@
+type ServiceSectionProps = {
+  id?: number;
+  title: string;
+  subTitle: string;
+  imgPath: string;
+  imgAlt: string;
+  text: string;
+};
+
+type BlogSiteSectionProps = {
+  id?: number;
+  imgPath: string;
+  alt: string;
+  date: string;
+  linkHref: string;
+  text: string;
+};
+
+type ProductionProps = {
+  linkHref: string;
+  imgPath: string;
+  alt: string;
+  title: string;
+  skills?: string;
+  topText?: string;
+  pageText?: string;
+  home?: boolean;
+};
+
+type skillArrayType = {
+  title: string;
+  skills: string[];
+};
+
+type iconsArrayType = {
+  href: string;
+  src: string;
+  alt: string;
+  className: string;
+};
